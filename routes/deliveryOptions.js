e3015d7d43caeefa25178cb256ba1ee51e1487bf
SuperSimpleDev Tester <supersimpledev.tester1@gmail.com>
2025-03-10 00:37:56 -0400

    Add expand=estimatedDeliveryTime to delivery-options API

--- routes/deliveryOptions.js
+++ routes/deliveryOptions.js
@@ -4,8 +4,21 @@ import { DeliveryOption } from '../models/DeliveryOption.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+  const expand = req.query.expand;
   const deliveryOptions = await DeliveryOption.findAll();
-  res.json(deliveryOptions);
+  let response = deliveryOptions;
+
+  if (expand === 'estimatedDeliveryTime') {
+    response = deliveryOptions.map(option => {
+      const deliveryTimeMs = Date.now() + option.deliveryDays * 24 * 60 * 60 * 1000;
+      return {
+        ...option.toJSON(),
+        estimatedDeliveryTimeMs: deliveryTimeMs
+      };
+    });
+  }
+
+  res.json(response);
 });
 
 export default router;
